3bb0f9da7abb169c66ed780a8f6bc873e18733ba
RandyGoldsmith <rgoldshz@gmail.com>
2018-08-22 01:33:15 -0500

    started youtube app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = "AIzaSyC7VlFxm3YkcpZ0SU28tK-TTd3atygcc34"; 
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+	
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+
+	render() {
+		return (
+		<div>
+			<SearchBar />
+		</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
